06cae9588d49876e1fa6781c0354c8dcb240ee86
Johnathon E. Wright <jw@mustmodify.com>
2012-03-22 16:38:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RbxCap::Application.config.secret_token = '661d67e080fd55e9e60ee3e17e93d0222264d82516fb60b55c6aab5a1cb8088404dd34cd5efd59c50bbec1e7a9963d2df607606c003541b5bb83fe6ff8af3890'
